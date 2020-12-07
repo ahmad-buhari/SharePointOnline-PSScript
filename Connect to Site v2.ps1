cf8b9cdbe10d23276ab1b40199770ab7ba3fe7d5
ahmad-buhari <at.buhari@gmail.com>
2020-12-06 16:49:32 -0800

    added selection 8, 10

--- Connect to Site v2.ps1
+++ Connect to Site v2.ps1
@@ -42,32 +42,34 @@ $collab = '00bfea71-4ea5-48d4-a4ad-7ea5c011abe5'
 function PowerShell-Menu {
 
 
-    #Menu options
-    param ([string]$Title = '')
+                            #Menu options
+                            param ([string]$Title = '')
 
-    #clear screen
-    Clear-Host
+                            #clear screen
+                            Clear-Host
 
-    #Outpput Menu Title & Menu Selection
-    Write-Host "---$Title---
+                            #Outpput Menu Title & Menu Selection
+                            Write-Host "---$Title---
 
-        `n
-        `n Press 1 - Verfiy SharePoint Pnp Module
-        `n Press 2 - Connect to SharePoint Page
-        `n Press 3 - Enable Site Features (Collaboration, Enterprise, Feed, Pages & Publishing )
-        `n Press 4 - Get current Site Features
-        `n Press 5 - Example Project Example Site
-        `n Press 6 - Create Wiki Example Page
-        `n Press 8 - Disconnect for SharePoint Page
-        `n Press 9 - Quit   
-     
-        " -ForegroundColor Yellow
+                                `n
+                                `n Press 1 - Verfiy SharePoint Pnp Module
+                                `n Press 2 - Connect to SharePoint Page
+                                `n Press 3 - Enable Site Features (Collaboration, Enterprise, Feed, Pages & Publishing )
+                                `n Press 4 - Get current Site Features
+                                `n Press 5 - Create Example Site
+                                `n Press 6 - Get all List pages
+                                `n Press 7 - Convert all List pages to Modern Experinces
+                                `n Press 8 - Menu Options
+                                `n Press 9 - Disconnect for SharePoint Page
+                                `n Press 10 - Quit   
+                                
+                                " -ForegroundColor Yellow
               
-}
+                        }
 
     
 #Title for Menu        
-PowerShell-Menu -Title 'SharePoint Unattended Commands'
+PowerShell-Menu -Title "SharePoint Unattended Commands"
 
        
         
@@ -76,12 +78,11 @@ do {
         
     $Path = $PSScriptRoot 
     $selection = Read-Host -Prompt "`n Please Select the following options"
-        
+   
                 
     # Iterating through loop
     Try { 
-        switch ([int]$selection) 
-        {
+        switch ([int]$selection) {
                         
             '1' { Get-Module -Name SharePointPnPPowerShellOnline } 
 
@@ -102,20 +103,70 @@ do {
                 Enable-PnPFeature -Identity $collab ;
             }
 
-            '4' { Write-Host "Current Site Feature" ;  
-
-                Get-PnPFeature ;
+            '4' {
+                    Write-Host "Current Site Feature" ;  
+                    Get-PnPFeature ;
             }
 
             '5' {
+                #var for example comments
+                $entry1 = "This is a text web part in a one column section. You can click inside this text block when in Edit mode to make changes. Besides being able to change the text, you can use the toolbar to choose styles, make other format changes, and add links. You can even add a table by clicking the ellipses (…) in the toolbar for more options."
+                $entry2 = " Hello! This is a Text web part in one of two columns in this section. You can click inside this text block when in Edit mode to make changes. Next to this paragraph is a column that contains an image web part. Click the image, and you can use the toolbar to change the image, add a link, crop the image, and more. Learn more about the text web part and the image web part. When you're done editing this page, you can click Save as draft to save your changes and leave edit mode. Only people with edit permissions on your site will be able to see it. If you are ready to make this page visible to everyone who can view your site, click Publish or Post news. For more information, see What happens when I publish a page?  " 
+                $entry3 = "This is the other web part of the two column"
 
-                Add-PnPFile -Path .\Example-Page.aspx -Folder $site/SitePages
+
+                #add client side modern page
+                Add-PnPClientSidePage -Name "Example-Page" ;
+
+                #adding section within page
+                Add-PnPClientSidePageSection -Page "Example-Page" -SectionTemplate OneColumn  ; 
+                Add-PnPClientSidePageSection -Page "Example-Page" -SectionTemplate TwoColumn  ; 
+
+                #inserting texts
+                Add-PnPClientSideText -Page "Example-Page" -Text $entry1 -Section 1 -Column 1 ;
+                Add-PnPClientSideText -Page "Example-Page" -Text $entry2 -Section 2 -Column 1 ;
+                Add-PnPClientSideText -Page "Example-Page" -Text $entry3 -Section 2 -Column 2 ;
+            }
+
+            '6' { 
+                    Write-Host "Current List of SharePoint" ;
+                    Get-PnPList | select Title, ListExperienceOptions | Out-GridView
+                 }   
+
+            '7' {
+
+                $SharePointList = Get-PnPList | foreach { $_.Id }
+                foreach ($list in $SharePointList) {
+                    Set-PnPList -Identity $list -ListExperience NewExperience -ErrorAction SilentlyContinue
+                }
+
 
             }
 
-            '8' {Disconnect-PnPOnline}
 
-            '9' { Write-Host " `n Exiting Window `n " -ForegroundColor Red }
+            '8' {
+                    Clear-Host;
+
+                    Write-Host " SharePoint Unattended Commands
+
+                        `n
+                        `n Press 1 - Verfiy SharePoint Pnp Module
+                        `n Press 2 - Connect to SharePoint Page
+                        `n Press 3 - Enable Site Features (Collaboration, Enterprise, Feed, Pages & Publishing )
+                        `n Press 4 - Get current Site Features
+                        `n Press 5 - Create Example Site
+                        `n Press 6 - Get all List pages
+                        `n Press 7 - Convert all List pages to Modern Experinces
+                        `n Press 8 - Menu Options
+                        `n Press 9 - Disconnect for SharePoint Page
+                        `n Press 10 - Quit 
+        
+                        " -ForegroundColor Yellow ;
+                }
+
+            '9' { Disconnect-PnPOnline }
+
+            '10' { Write-Host " `n Exiting Window `n " -ForegroundColor Red }
         
         }
 
@@ -124,12 +175,17 @@ do {
     catch {
         [System.OutOfMemoryException]
                     
-        Write-Host 'Restart Powershell script' -ForegroundColor Red
-    }
-                
-    if ($selection -gt 9) { Write-Host "Please Choose Again" -ForegroundColor Red }
+        Write-Host 'Restart Powershell script' -ForegroundColor Red; 
 
+    }
+
+    if ($selection -gt 10) { Write-Host "`n Please Choose Options" -ForegroundColor Red }
                 
-} until ($selection -eq 9)
+    
+    
+                
+} until ($selection -eq 10)
+
+
 
                 
