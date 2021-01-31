f9ce4d950faf0ac15e89059c89d5b944478f1c28
ahmad-buhari <at.buhari@gmail.com>
2021-01-31 13:17:57 +0000

    updated feature10, corrected ErrorOutput, inserted reference comments

--- Connect to Site v2.ps1
+++ Connect to Site v2.ps1
@@ -196,64 +196,61 @@ do {
 } until ($selection -eq 11)
 
 
-#Intro
-#Spaces are not friendly redirect to URL
+####Feature10 in-progress####
+
+#Naming new sharepoint list
+#Lessons: Spaces are not friendly when redirecting to a web browser
 $project = Read-Host -Prompt "Enter Project Name"
 $project0 = "$project-List"
-Write-Host $project0
+$new = New-PnPList -Title "$project0" -Template GenericList 
 
-New-PnPList -Title "$project0" -Template GenericList ;
-
-
-
-#Only one xml attribute can be called at a time
-
-$TaskXml = '<Field Type="Text" Name="Task" DisplayName="Task" Viewable="TRUE" />
-
-'
+Write-Host "Building List..." -ForegroundColor Yellow
 
 
 
+#Lessons: Identical xml attribute can only be assigned at a single time. Using variables to separate each new column
+
+    $col1TaskXml = '<Field Type="Text" Name="Task" DisplayName="Task" Viewable="TRUE" />'
 
 
-$StatusXml = '
-        <Field Type="Choice" Name="Status" DisplayName="Status" Viewable="TRUE">
-            <CHOICES>
-                <CHOICE>Not Started</CHOICE>
-                <CHOICE>In Progress</CHOICE>
-                <CHOICE>Completed</CHOICE>
-                <CHOICE>Deferred</CHOICE>
-            </CHOICES>
-            <Default>Not Started</Default>
-        </Field>
+    $col2StatusXml = '
+                    <Field Type="Choice" Name="Status" DisplayName="Status" Viewable="TRUE">
+                        <CHOICES>
+                            <CHOICE>Not Started</CHOICE>
+                            <CHOICE>In Progress</CHOICE>
+                            <CHOICE>Completed</CHOICE>
+                            <CHOICE>Deferred</CHOICE>
+                        </CHOICES>
+                        <Default>Not Started</Default>
+                    </Field>
+                '   
 
-'
+#Customizing Column 1
+$col1 = Add-PnPFieldFromXml -List "$project0" -FieldXml $TaskXml;
 
+Write-Host "Customizing..." -ForegroundColor Yellow;
 
+#Customizing Column 2
+$col2 = Add-PnPFieldFromXml -List "$project0" -FieldXml $StatusXml;
 
-#feature10
-
-Add-PnPFieldFromXml -List "$project0" -FieldXml $TaskXml ;
-
-Start-Sleep -s 3 ;
-
-Add-PnPFieldFromXml -List "$project0" -FieldXml $StatusXml ;
-
-Start-Sleep -s 5 ;
-
-#List point to the acutal list, Identity refers to the "View" name, Fields refers to the coloumn (multiple coulmns can be called)
+#Clarity: "List" points to the acutal list name within SharePint, "Identity" refers to the "View" name of the list (deafult is "All items"), Fields refers to the columns (multiple coulmns can be called)
 
 #Add Custom View
-Set-PnPView -List "$project0" -Identity "All Items" -Fields "Task", "Status" -ErrorAction SilentlyContinue ;
+$view = Set-PnPView -List "$project0" -Identity "All Items" -Fields "Task", "Status" -ErrorAction Ignore ;
 
-#Waiting for html to build
-Start-Sleep -s 5 ;
+Write-Host "Updating New Look..." -ForegroundColor Yellow
 
+#Pauses for code to inject in SharePoint
+Start-Sleep -s 2 ;
 
+#Retriving new list url
 $project1 = Get-PnPList "$project0" ;
 $parent = "https://afnet52.sharepoint.com" ;
 $full = ($parent + $project1.DefaultViewUrl) ;
 
 #opens in web browser
-Start-Process -FilePath Chrome $full
+Write-Host "`nNew Project Page Url:" $full -ForegroundColor Yellow 
+Write-Host "Loading Page..." -ForegroundColor Yellow
+
+$newList = Start-Process -FilePath Chrome $full
                 
